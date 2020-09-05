3764d2b328e4497dbf69927199bc31c400112dd3
Alex Alexeyev <aasmc@yandex.ru>
2020-09-05 23:58:10 +0300

    Update dependencies

--- app/src/main/java/com/smartherd/globofly/activities/DestinationDetailActivity.kt
+++ app/src/main/java/com/smartherd/globofly/activities/DestinationDetailActivity.kt
@@ -2,7 +2,7 @@ package com.smartherd.globofly.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.MenuItem
 import android.widget.Toast
 import com.smartherd.globofly.R
